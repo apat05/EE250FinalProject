35e208086af9ac6f4d03b3fdb93754bd6faa51f2
Aum Patel <aumpatel@usc.edu>
2023-05-02 02:57:32 -0700

    changes

--- laptop.py
+++ laptop.py
@@ -10,27 +10,29 @@ response_topic = "door/response"
 # Callback function for MQTT message received
 def on_message(client, userdata, message):
     if message.topic == sensor_topic:
-        # Print distance measurement received from Raspberry Pi
-        distance = int(message.payload)
-        print("Distance: {} cm".format(distance))
+        distance = int(message.payload.decode())
+        if distance < 50:
+            # Request access from owner if someone is near the door
+            response = input("Someone is at the door. Request access? (yes/no): ")
 
-        # Prompt user for response
-        response = input("Approve access? (yes/no): ")
+            # Send response to MQTT broker
+            mqtt_client.publish(response_topic, response)
 
-        # Send response to Raspberry Pi
-        mqtt_client.publish(response_topic, response)
-
-# Connect to MQTT broker
-mqtt_client.connect("172.20.10.9", 1883, 60)
-
-# Subscribe to sensor topic
-mqtt_client.subscribe(sensor_topic)
-
-# Set up callback function for MQTT message received
+# Connect to MQTT broker and subscribe to sensor topic
 mqtt_client.on_message = on_message
+mqtt_client.connect("172.20.10.9", 1883, 60)
+mqtt_client.subscribe(sensor_topic)
+mqtt_client.loop_start()
 
-# Loop to receive responses from user
-mqtt_client.loop_forever()
+# Loop to receive responses from owner
+while True:
+    try:
+        # Wait for response from owner
+        mqtt_client.loop(timeout=1.0)
 
-# Disconnect from MQTT broker
+    except KeyboardInterrupt:
+        break
+
+# Disconnect from MQTT broker and stop loop
+mqtt_client.loop_stop()
 mqtt_client.disconnect()
