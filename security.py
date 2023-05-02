d3f763b4a379be82f7a5e843ed0a8771762ef934
Aum Patel <aumpatel@usc.edu>
2023-05-02 02:40:54 -0700

    changes

--- security.py
+++ security.py
@@ -20,6 +20,21 @@ mqtt_client.connect("172.20.10.9", 1883, 60)
 # Initialize the previous distance measurement
 prev_distance = -1
 
+# Turn on the LEDs appropriately
+# Callback function for MQTT message received
+def on_message(client, userdata, message):
+    if message.topic == response_topic:
+        if message.payload.decode() == "yes":
+            # Turn on green LED if owner approves
+            grovepi.digitalWrite(green_led_port, 1)
+            grovepi.digitalWrite(red_led_port, 0)
+            print("Owner approved access!")
+        else:
+            # Turn on red LED if owner denies access
+            grovepi.digitalWrite(green_led_port, 0)
+            grovepi.digitalWrite(red_led_port, 1)
+            print("Owner denied access!")
+
 # Loop to read ultrasonic sensor and send MQTT messages
 while True:
     try:
@@ -27,7 +42,7 @@ while True:
         distance = grovepi.ultrasonicRead(ultrasonic_sensor_port)
 
         # Check if distance has changed since previous measurement
-        if distance != prev_distance:
+        if distance <= prev_distance - 10:
             # Send distance measurement to MQTT broker
             mqtt_client.publish(sensor_topic, distance)
 
