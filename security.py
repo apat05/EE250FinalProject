da25a20ca32f0b8f4bed3f2224f476174b2f73fe
Aum Patel <aumpatel@usc.edu>
2023-05-02 02:18:41 -0700

    change

--- security.py
+++ security.py
@@ -15,7 +15,7 @@ sensor_topic = "door/sensor"
 response_topic = "door/response"
 
 # Connect to MQTT broker
-mqtt_client.connect("172.20.10.6", 11000, 60)
+mqtt_client.connect(host="172.20.10.9", port=11000, keepalive=60)
 
 # Initialize the previous distance measurement
 prev_distance = -1
